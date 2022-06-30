633083dc7d0f0dde9f5c677eae905ec00a7f4008
ibkbaba <you@example.com>
2022-06-30 16:12:58 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,19 @@
-include <stdio.h>
-/
-*main-entry point
-*return:always 0(sucess)
-*/
-int main(void)
-{
-put("\"Programming is like building a multilingual puzzle");
-return (0);
-}	
+include <stdio.h>                                                                                                                                               
+
+/                                                                                                                                                               
+
+*main-entry point                                                                                                                                               
+
+*return:always 0(sucess)                                                                                                                                        
+
+*/                                                                                                                                                              
+
+int main(void)                                                                                                                                                  
+
+{                                                                                                                                                               
+
+	put("\"Programming is like building a multilingual puzzle");                                                                                                    
+
+	return (0);                                                                                                                                                     
+
+}                     

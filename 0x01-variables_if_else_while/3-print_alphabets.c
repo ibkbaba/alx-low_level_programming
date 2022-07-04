6abe261746c427a445a00b05aad5fa0a5aab7b7d
ibkbaba <you@example.com>
2022-07-04 09:06:21 -0700

    3-print_alphabets.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-/**assign a random number to the variable n each time it is executed. 
+/**
+ *main-assign a random number to the variable n each time it is executed.
  *print the last digit of the number stored in the variable n
  *return : always 0
-*/
+ */
 int main(void)
-int ch;
-for (ch = 97; ch <=122; ch++)
 {
-	putchar(ch);
-}
-for (ch = 65; ch <=90; ch++)
-{
-	putchar(ch);
-}
-putchar(10); /*this is an ascii code for new line*/
+	int ch;
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 65; ch <= 90; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10); /*this is an ascii code for new line*/
 
-return (0);
+	return (0);
+}

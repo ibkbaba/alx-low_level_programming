dea117b0e6aa653140ea797653145e372a97cb1d
ibkbaba <you@example.com>
2022-07-04 14:35:09 -0700

    number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,11 +3,10 @@
 /**
  *main-assign a random number to the variable n each time it is executed
  *print the last digit of the number stored in the variable n
- *return : always 0
+ *Return:always 0
 */
 int main(void)
 {
-
 	int n;
 
 	for (n = 0; n < 10; n++)

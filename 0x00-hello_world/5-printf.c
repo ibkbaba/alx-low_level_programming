6530ece95950bebddce87c05d94f593d435a47ee
ibkbaba <you@example.com>
2022-07-04 13:28:09 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,11 @@
-include <stdio.h>                                                                                                                                               
+#include< stdio.h >
+/**
+*main-entry point
+*Return: always 0 (success)
+*/
 
-/                                                                                                                                                               
-
-* main - entry point                                                                                                                                            
-*
-* * return: always 0 (success)                                                                                                                                    
-*
-* */                                                                                                                                                              
-
-int main(void)                                                                                                                                                  
-
-{                                                                                                                                                               
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");                                                                                            
-
-	return (0);                                                                                                                                                     
-
-}                          
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

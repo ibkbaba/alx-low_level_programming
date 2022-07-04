360070f9de41fb750de44ca7d015e59aca9cd83b
ibkbaba <you@example.com>
2022-07-04 11:47:08 -0700

    vi 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
-/**assign a random number to the variable n each time it is executed. 
+/**
+ *main-assign a random number to the variable n each time it is executed
  *print the last digit of the number stored in the variable n
  *return : always 0
 */
 int main(void)
 {
-	(int n;)
-	 for (n = 0; n<10; n++)
-	 
-	if (ch ! = 'q' && ch != 'e')
-	{
-		putchar(ch);
-	}
-	putchar(10); /* this is an ascii code */
 
+	(int n;)
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
 
 return (0);
 }

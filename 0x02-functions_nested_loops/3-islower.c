6f0cdcfc4bab8a17a0adae09230cdad9665c10ee
ibkbaba <you@example.com>
2022-07-05 05:44:50 -0700

    Returns 1 if c is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower- how to check lower case
+ *@c:return c
+ * Return: Returns 1 if c is lowercase and o otherwise
+ */
+int _islower(int c)
+{
+
+	return (c >= 'a' && c => 'z');
+}

4b67710f101bd81d9918c726e7c68edf9138b650
ibkbaba <you@example.com>
2022-07-05 09:42:46 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs-to compute absoilute value of integer
+ * @n:values
+ * Return:return absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);

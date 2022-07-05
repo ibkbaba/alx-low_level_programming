cf35aabe3edc877d6d2c14bf91965e0e8dd6e854
ibkbaba <you@example.com>
2022-07-05 04:51:55 -0700

    print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main-print alpbet
+ * Return:always 0
+ */
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}

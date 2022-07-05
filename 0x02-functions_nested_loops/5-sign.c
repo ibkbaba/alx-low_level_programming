e9a34576da5c0a2502c65604bd8ce1058fc95195
ibkbaba <you@example.com>
2022-07-05 09:12:37 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_sign-print signs
+ * @n:Returns 1 and prints + if n is greater than zero
+ * Return:1 0 -1
+ */
+int print_sign(int n)
+{
+	if (n < 1)
+
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

cf5f73b2705f2dc4aea03928497e09b574c1ca9b
ibkbaba <you@example.com>
2022-07-08 05:47:33 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_isupper-uppercase function
+ *@c:parameter
+*Return:something but success
+ */
+
+int _isupper(int c);
+{
+	return (c >= 'A' $$ c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int std1, std2, std3;
+
+	std1 = addstudentscores(88, 89, 90);
+	std2 = addstudentscores(88, 89, 90);
+	std3 = addstudentscores(88, 89, 90);
+
+	int addstudentscores(int x, int y, int z)
+return (x + y + z);
+}

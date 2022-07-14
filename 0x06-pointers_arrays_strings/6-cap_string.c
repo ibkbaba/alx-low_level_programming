9fb06a2dff7f7faa3f0e642556dd33ef19b1a5bf
ibkbaba <you@example.com>
2022-07-14 09:03:21 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,31 +2,30 @@
 
 /**
  * cap_string-function that capitalizes all words of a string.
- * @str: string to capitalise
+ * @s: string to capitalise
  * Return:capital string
  */
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int x = 0;
+	int count = 0, i;
+	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		while (!(str[++x] >= 'a' && str[x] <= 'z'))
-			x++;
-		if (str[x - 1] == ' ' ||
-		str[x - 1] == '\t' ||
-		str[x - 1] == '\n' ||
-		str[x - 1] == ',' ||
-		str[x - 1] == ';' ||
-		str[x - 1] == '.' ||
-		str[x - 1] == '!' ||
-		str[x - 1] == '?' ||
-		str[x - 1] == '"' ||
-		str[x - 1] == '(' ||
-		str[x - 1] == ')' ||
-		str[x - 1] == '{' ||
-		str[x - 1] == '}')
-		str[x] -= 32;
+		for (i = 0; i < 13; i++)
+		{
+			if (*(s + count) == separators[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
+		}
+		count++;
 	}
-	return (str);
+	return (s);
 }

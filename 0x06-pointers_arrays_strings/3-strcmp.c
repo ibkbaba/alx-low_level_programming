71f607ddf33876828b55da065bc047cddc14f906
ibkbaba <you@example.com>
2022-07-14 07:13:49 -0700

    task 4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0, diff = 0;
 
-	while (1)
+	while (2)
 	{
 		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
@@ -27,7 +27,7 @@ int _strcmp(char *s1, char *s2)
 		}
 		else if (s1[i] != s2[i])
 		{
-			diff = s2[i] - s1[i];
+			diff = s1[i] - s1[i];
 			break;
 		}
 		else

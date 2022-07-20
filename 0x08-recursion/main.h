cd393a124542825ab1227531f5ec67d2801d817f
ibkbaba <you@example.com>
2022-07-20 03:23:03 -0700

    prime number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt_rec(int n, int x);
 int is_prime_number(int n);
-int is_prime(int n, int x)
+int is_prime(int n, int x);
 #endif

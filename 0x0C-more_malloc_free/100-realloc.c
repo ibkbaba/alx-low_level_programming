967b7c2441b1b8b4e19b741e4098a5af57d1d151
ibkbaba <you@example.com>
2022-07-28 12:17:43 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,25 +16,22 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr != NULL)
 		clone = ptr;
-		{
-			p = malloc(new_size);
-			return (p);
-		}
-		else if (new_size == 0)
+		else
+		{ return (malloc(new_size)); }
+		if (new_size == old_size)
+			return (ptr);
+		if (new_size == 0 && ptr != NULL)
 		{
 			free(ptr);
-			return (NULL);
+			return (0);
 		}
-		else if (new_size == old_size)
-			return (ptr);
-
-		p = malloc(new_size);
-		if (p == NULL)
-			return (NULL);
-		if (new_size > old_size)
-			max = old_size;
-		for (i = 0; i < max; i++)
-			p[i] = oldp[i];
+		relloc = malloc(new_size);
+		if (relloc == NULL)
+			return (0);
+		for (i = 0; i < (old_size || i < new_size); i++)
+		{
+			*(relloc + i) = clone[i];
+		}
 		free(ptr);
-		return (p);
+		return (relloc);
 }

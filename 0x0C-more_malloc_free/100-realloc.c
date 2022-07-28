da24da9092961fb7b79e213719f022ed09ad447e
ibkbaba <you@example.com>
2022-07-28 11:33:10 -0700

    realoc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,29 +10,28 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-		char *p;
-		unsigned int i, max = new_size;
-		char *oldp = ptr;
-
-		if (ptr == NULL)
+	char *p;
+	unsigned int i, max = new_size;
+	char *oldp = ptr;
 		{
 			p = malloc(new_size);
 			return (p);
 		}
 		else if (new_size == 0)
 		{
-		free(ptr);
-		return (NULL);
+			free(ptr);
+			return (NULL);
 		}
 		else if (new_size == old_size)
-		return (ptr);
+			return (ptr);
+
 		p = malloc(new_size);
-		if (p == NULL
-		return (NULL);
+		if (p == NULL)
+			return (NULL);
 		if (new_size > old_size)
-		max = old_size;
+			max = old_size;
 		for (i = 0; i < max; i++)
-		p[i] = oldp[i];
+			p[i] = oldp[i];
 		free(ptr);
 		return (p);
 }

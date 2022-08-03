4d6b5f28131637eff7abcaf0325ed4465a63b498
ibkbaba <you@example.com>
2022-08-03 11:11:39 -0700

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+
+
+
+/**
+ * int_index - searches for an integer
+ * @array: array to serch through
+ * @size: size of array
+ * @cmp: function used to compare
+ * Return: first index for which cmp doesn't return 0, or -1

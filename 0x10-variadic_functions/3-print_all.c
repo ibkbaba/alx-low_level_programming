f1def5bce454b8bfb9429e4f3096ad8ee4e7789d
ibkbaba <you@example.com>
2022-08-04 15:04:07 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,9 +3,9 @@
 #include "variadic_functions.h"
 
 /**
- * print_c - print a char
- * @c: char to print
- * Return: void
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
+ * Return: no return
  */
 void print_all(const char * const format, ...)
 {
